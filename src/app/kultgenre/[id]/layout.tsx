d4893f7487c04d1d8c2c814542430b60bed92312
PatriciJo <sebastian.patrici@gmail.com>
2025-04-12 18:33:06 +0200

    refactor(layout): remove unused searchParams from Props type
    The searchParams property in the Props type was unused and unnecessary, so it has been removed to simplify the code and improve maintainability.

--- src/app/kultgenre/[id]/layout.tsx
+++ src/app/kultgenre/[id]/layout.tsx
@@ -12,9 +12,6 @@ type Props = {
   params: Promise<{
     id: string;
   }>;
-  searchParams?: Promise<{
-    [key: string]: string | string[] | undefined;
-  }>;
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
